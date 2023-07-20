6c0ec726165bc69ef23381bee65f2395b1e8219f
动漫模块 <130392956+KoDelioDa@users.noreply.github.com>
2023-07-20 13:19:52 -0700

    Update 0-MotionModule.sh

--- AnimateDiff/0-MotionModule.sh
+++ AnimateDiff/0-MotionModule.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
-
-# 定义文件下载链接数组
-URLS=("https://huggingface.co/godlzj/Motion_Module/resolve/main/mm_sd_v14.ckpt" 
-      "https://huggingface.co/godlzj/Motion_Module/resolve/main/mm_sd_v15.ckpt")
-
-# 定义目标文件夹
-DEST_DIR="models/Motion_Module"
-
-# 创建目标文件夹（如果不存在）
-mkdir -p "$DEST_DIR"
-
-# 使用wget命令下载文件到目标文件夹
-for url in "${URLS[@]}"
-do
-    wget "$url" -P "$DEST_DIR"
-done
+wget https://huggingface.co/godlzj/Motion_Module/resolve/main/mm_sd_v14.ckpt -p models/Motion_Module/ --content-disposition --no-check-certificate
+wget https://huggingface.co/godlzj/Motion_Module/resolve/main/mm_sd_v15.ckpt -p models/Motion_Module/ --content-disposition --no-check-certificate
