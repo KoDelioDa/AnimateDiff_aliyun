0db8b75f43a41b74d8f46de5a878daa8dde8e436
动漫模块 <130392956+KoDelioDa@users.noreply.github.com>
2023-07-20 12:44:44 -0700

    Update 1-ToonYou.sh

--- AnimateDiff/download_bashscripts/1-ToonYou.sh
+++ AnimateDiff/download_bashscripts/1-ToonYou.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget https://liblibai-online.liblibai.com/models/0985a1209b684e081a64016d77ba8606c0de620c.safetensors?attname=ToonYou_Beta1.safetensors -P models/DreamBooth_LoRA/ --content-disposition --no-check-certificate
+wget https://liblibai-online.liblibai.com/models/0985a1209b684e081a64016d77ba8606c0de620c.safetensors?attname=ToonYou_Beta1.safetensors -P models/DreamBooth_LoRA/
