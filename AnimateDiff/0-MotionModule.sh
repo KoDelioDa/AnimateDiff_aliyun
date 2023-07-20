6e2fbfed7c82a73126bc8f202fce2d9db3407496
动漫模块 <130392956+KoDelioDa@users.noreply.github.com>
2023-07-20 13:33:25 -0700

    Update 0-MotionModule.sh

--- AnimateDiff/0-MotionModule.sh
+++ AnimateDiff/0-MotionModule.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://huggingface.co/godlzj/Motion_Module/resolve/main/mm_sd_v14.ckpt -p models/Motion_Module/ --content-disposition --no-check-certificate
-wget https://huggingface.co/godlzj/Motion_Module/resolve/main/mm_sd_v15.ckpt -p models/Motion_Module/ --content-disposition --no-check-certificate
+wget https://huggingface.co/godlzj/Motion_Module/resolve/main/mm_sd_v14.ckpt -P models/Motion_Module/ --content-disposition --no-check-certificate
+wget https://huggingface.co/godlzj/Motion_Module/resolve/main/mm_sd_v15.ckpt -P models/Motion_Module/ --content-disposition --no-check-certificate
